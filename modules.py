27b197a69c852ea5a051341c9f0d5d1c8ff1746f
englola <129763197+englola@users.noreply.github.com>
2023-04-12 09:34:13 +0200

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,4 @@
+from pybricks import robotics
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor
+from pybricks.parameters import Port, Stop, Direction, Color
